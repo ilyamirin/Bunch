f25712332b1624f5c8044865846ade633e639280
Ilya G. Mirin <mirin@dvc.ru>
2010-11-11 11:56:47 +1000

    Изменил способ вызова методов банча из контроллера

--- Bunch.pm
+++ Bunch.pm
@@ -6,26 +6,29 @@ class Catalyst::Plugin::Bunch {
     has md5 => ( is => 'ro', isa => 'Digest::MD5', 
         default => sub { Digest::MD5->new } );
 
-    method _load ( $c, $type, $files ) {
+    has c => ( is => 'rw', isa => 'Object' );
 
-        my $config = $c->config->{ Bunch };
+    method _load ( $type, $files ) {
 
-        my $model = $c->model( $config->{ model}->{ $type } );
+        my $config = $self->c->config->{ Bunch };
+
+        my $model = $self->c->model( $config->{ model}->{ $type } );
        
         my $default = $config->{ default_libs }->{ $type };
         
         my $text;
         foreach ( ( @$default, @$files ) ) {
             eval { $text .= $model->load( $_ ); };
-            $c->log->error( $@ ) if $@;
+            $self->c->log->error( $@ ) if $@;
         }
 
         use Digest::MD5 qw/ md5_hex /;
         my $md5 = md5_hex( $text . $config->{ minify } );
 
         if ( my $file = $model->exist( "bunch/$md5" ) ) {
-            $c->stash->{ static }->{ $type } = $model->url_to("bunch/$md5");
-            $c->log->info("Банч $md5 типа $type загружен." );
+            $self->c->stash->{ static }->{ $type } = 
+                $model->url_to("bunch/$md5");
+            $self->c->log->info("Банч $md5 типа $type загружен." );
         } 
         else {
             if ( $config->{ minify } ) {
@@ -35,21 +38,37 @@ class Catalyst::Plugin::Bunch {
                 $text = minify( $text ) ;
             }
             $model->save( "bunch/$md5", $text );
-            $c->stash->{ static }->{ $type } = $model->url_to("bunch/$md5");
-            $c->log->info("Банч $md5 типа $type создан и загружен." );
+            $self->c->stash->{ static }->{ $type } = 
+                $model->url_to("bunch/$md5");
+            $self->c->log->info("Банч $md5 типа $type создан и загружен." );
         } 
 
-        return $c->stash->{ static }->{ $text };
+        return $self->c->stash->{ static }->{ $text };
 
     }#_load
 
-    method load_static ( $c ) {
+    sub bunch {
+        return Catalyst::Plugin::Bunch->new( c => shift );
+    }
+
+    method load_static {
         
-        while ( my ( $k, $v ) = each %{ $c->stash->{ static } } ) {
-            $self->_load( $c, $k, $v );
+        while ( my ( $k, $v ) = each %{ $self->c->stash->{ static } } ) {
+            $self->_load( $k, $v );
         }
 
-   }#load_static
+    }#load_static
+
+    sub add_css {
+        my ( $c, $file ) = @_;
+
+        $c->log->info( $file );
+
+        push @{ $c->stash->{ static }->{ css } }, $file;
+
+        $c->log->info( $_ ) foreach @{ $c->stash->{ static }->{ css } };
+
+    }#add_static
 
 }#class
 
