29361d063c1b74f16a89b91e2d6a9b6e5eb4285d
Ilya G. Mirin <mirin@dvc.ru>
2010-11-12 11:45:39 +1000

    переместил хранилище ссылок на статику для банча и ссылок на сами банчи из стэша в поле банча

--- Bunch.pm
+++ Bunch.pm
@@ -9,15 +9,19 @@ has md5 => ( is => 'ro', isa => 'Digest::MD5',
 
 has c => ( is => 'rw', isa => 'Object' );
 
+has store => ( is => 'rw', isa => 'HashRef', default => sub { { } } );
+
+has static => ( is => 'rw', isa => 'HashRef', default => sub { { } } );
+
 sub _load {
     my ( $self, $type, $files ) = @_;
 
     my $config = $self->c->config->{ Bunch };
 
-    my $model = $self->c->model( $config->{ model}->{ $type } );
+    my $model = $self->c->model( $config->{ model }->{ $type } );
        
     my $default = $config->{ default_libs }->{ $type };
-        
+         
     my $text;
     foreach ( ( @$default, @$files ) ) {
         eval { $text .= $model->load( $_ ); };
@@ -27,10 +31,8 @@ sub _load {
     my $md5 = md5_hex( $text . $config->{ minify } );
 
     if ( my $file = $model->exist( "bunch/$md5" ) ) {
-        $self->c->stash->{ static }->{ $type } = 
-            $model->url_to("bunch/$md5");
+        $self->static->{ $type } = $model->url_to("bunch/$md5");
         $self->c->log->info("Банч $md5 типа $type загружен." );
-
     } 
     else {
         if ( $config->{ minify } ) {
@@ -41,20 +43,19 @@ sub _load {
         }#if
 
         $model->save( "bunch/$md5", $text );
-        $self->c->stash->{ static }->{ $type } = 
-           $model->url_to("bunch/$md5");
+        $self->static->{ $type } = $model->url_to("bunch/$md5");
         $self->c->log->info("Банч $md5 типа $type создан и загружен." );
 
     } #else if
 
-    return $self->c->stash->{ static }->{ $text };
+    return $self->static->{ $text };
 
 }#_load
 
 sub load_static {        
     my $self = shift;
 
-    while ( my ( $k, $v ) = each %{ $self->c->stash->{ static } } ) {
+    while ( my ( $k, $v ) = each %{ $self->store } ) {
         $self->_load( $k, $v );
     }
 
@@ -67,7 +68,7 @@ sub AUTOLOAD {
 
     $AUTOLOAD =~ /^.+_(.+)$/;
 
-    push @{ $self->c->stash->{ static }->{ $1 } }, $file;
+    push @{ $self->store->{ $1 } }, $file;
 
 };#AUTOLOAD
 
