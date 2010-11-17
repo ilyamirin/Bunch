4774587155eb39e65c421d786b5347b62c03e1bc
Ilya G. Mirin <mirin@dvc.ru>
2010-11-17 19:08:32 +1000

    Упростил и усовершенствовал банч. Изменил спосо вставки статики в шаблон.

--- Bunch.pm
+++ Bunch.pm
@@ -2,33 +2,29 @@
 package Catalyst::Plugin::Bunch::Slave;
 use MooseX::Singleton;
 
-use Digest::MD5 qw/ md5_hex /;
-
-has md5 => ( is => 'ro', isa => 'Digest::MD5', 
-    default => sub { Digest::MD5->new } );
-
 has c => ( is => 'rw', isa => 'Object' );
 
-has store => ( is => 'rw', isa => 'HashRef', default => sub { { } } );
+has js => ( is => 'rw', isa => 'ArrayRef', default => sub { [ ] } );
 
-has static => ( is => 'rw', isa => 'HashRef', default => sub { { } } );
+has css => ( is => 'rw', isa => 'ArrayRef', default => sub { [ ] } );
 
-sub _load {
-    my ( $self, $type, $files ) = @_;
-
+sub load {
+    my ( $self, $type ) = @_;
+    
+    my $files = $self->$type;
+    
     my $config = $self->c->config->{ Bunch };
 
     my $model = $self->c->model( $config->{ model }->{ $type } );
 
-    my @lm = map { $model->last_modified ( $_ ) } @$files;
+    my @lm = map { $model->last_modified( $_ ) } @$files;
 
-#    $self->c->log->info( join ' ', ( @$files, @lm, $config->{ minify } ) );
-
+    use Digest::MD5 qw/ md5_hex /;
     my $md5 = md5_hex( join '', ( @$files, @lm, $config->{ minify } ) );
 
-    if ( my $file = $model->exist( "bunch/$md5" ) ) {
-        $self->static->{ $type } = $model->url_to("bunch/$md5");
+    if ( my $file = $model->exist( "bunch/$md5" ) ) {        
         $self->c->log->info("Банч $md5 типа $type загружен." );
+        return $model->url_to("bunch/$md5");
 
     } 
     else { 
@@ -36,7 +32,6 @@ sub _load {
 
         my $text;
         foreach ( ( @$default, @$files ) ) {
-            $self->c->log->info( $_ );
             eval { $text .= $model->load( $_ ); };
             $self->c->log->error( $@ ) if $@;
         }
@@ -49,40 +44,32 @@ sub _load {
         }#if
 
         $model->save( "bunch/$md5", $text );
-        $self->static->{ $type } = $model->url_to("bunch/$md5");
         $self->c->log->info("Банч $md5 типа $type создан и загружен." );
+        $self->$type( [ ] );
 
-    } #else if
+        return $model->url_to("bunch/$md5");
 
-}#_load
+    }#else if
 
-sub load_static {        
-    my $self = shift;
+}#load
 
-    while ( my ( $k, $v ) = each %{ $self->store } ) {
-        $self->_load( $k, $v );
-    }
+sub add {
+    my ( $self, $type, $file ) = @_;  
 
-    $self->store( {} );
+    push @{ $self->$type }, $file;
 
     return;
 
-}#load_static
+}#add
 
 sub AUTOLOAD {
-    my ( $self, $file, $priority ) = @_;  
+    my $self = shift;  
 
     our $AUTOLOAD;
 
-    $AUTOLOAD =~ /add_(.+)$/;
+    $AUTOLOAD =~ /(.+)_(.+)$/;
     
-    return unless $1;
-
-    $priority = 100 unless $priority;
-
-    push @{ $self->store->{ $1 } }, $file;
-
-    return;
+    $self->$1( $2, shift );
 
 }#AUTOLOAD
 
