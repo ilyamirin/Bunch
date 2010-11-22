0730c1c69125cadd22de1aa1c58efc1ab3e16166
Ilya G. Mirin <mirin@dvc.ru>
2010-11-22 18:54:10 +1000

    Начал переписывать кат под новую соап либу

--- SOAP.pm
+++ SOAP.pm
@@ -2,37 +2,16 @@
 package Catalyst::Plugin::SOAP::Slave;
 use MooseX::Singleton;
 
-use SOAP::Lite +trace => 'all';
-
-has params => ( is => 'rw', isa => 'Str', default => 'soap' );
-
-has readable => ( is => 'rw', isa => 'Bool', default => '1' );
+use SOAP::WSDL;
 
 has c => ( is => 'rw', isa => 'Object' );
 
-sub client {
-    my ( $self, $service ) = @_;
-
-    my $client = SOAP::Lite->service( 
-        $self->c->config->{ 'Plugin::SOAP' }->{ $service }->{ 'wsdl' } 
-    );
-
-    $client->readable( $self->readable );
-
-    $client->ns( 'http://schemas.xmlsoap.org/soap/envelope/', 'soap12' );
-
-    return $client;
-
-}#client
-
-sub send {        
+sub send {         
     my ( $self, $service, $method, $args ) = @_;
-    
-    my $client = $self->client( $service ); 
 
-    eval { $client->$method( @$args ); };
-
-    $self->c->log->error( $@ ) if $@;
+    SOAP::WSDL
+        ->new( wsdl => $service )
+        ->call( $method, $method => $args );
   
 }#send
 
@@ -43,11 +22,13 @@ sub AUTOLOAD {
 
     $AUTOLOAD =~ /\:\:([^\:]+)$/;
 
-    if ( $self->c->config->{ 'Plugin::SOAP' }->{ $1 } ) {
-        return $self->client( $1 ); 
+    my $service = $self->c->config->{ 'Plugin::SOAP' }->{ $1 };
+
+    if ( $service ) {
+#        return $self->send( $service, $2 ); 
     } 
     else {
-        $self->c->error( "Unknown SOAP service $1 !" );
+        $self->c->log->error( "Unknown SOAP service $1 !" );
     }
     
 }#AUTOLOAD
@@ -57,7 +38,7 @@ package Catalyst::Plugin::SOAP;
 sub SOAP {
     my $slave = Catalyst::Plugin::SOAP::Slave->instance;
 
-    $slave->c( shift ) unless defined $slave->c;
+    $slave->c( shift );
 
     return $slave;
 
